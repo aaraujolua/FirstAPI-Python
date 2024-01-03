ff1c545b73c15d2a7278354cc26d50581b4e1fbf
Luana Araújo <f.araujoluana@gmail.com>
2024-01-03 18:56:48 -0300

    COMPLETE main.py

--- venv/main.py
+++ venv/main.py
@@ -1,11 +1,27 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+
+class User(BaseModel):
+    name: str
+    user: Optional[str] = None
+    password: str
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/hello-world")
 def read_root():
-    return {"Hello": "World"}
+    return {"Msg": "Hello World, this is my first Python api"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/api/sum")
+def sum():
+    return {"Result": 4+3}
+
+
+@app.post("/api/login")
+def login(user: User):
+    if user.user == "lua" and user.password == "123lua":
+        return { "Msg": "Successful login!"}
+
+    else:
+        return { "Msg": "Login or password incorrect!"}
